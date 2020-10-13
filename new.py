dc3a8ef395a5d439a53936e8fd3cabf5145185ee
Julia Henke <jhenke2022@gmail.com>
2020-10-13 11:01:07 -0400

    repmat trail

--- new.py
+++ new.py
@@ -1,2 +1,10 @@
-print('this is a new file')
-new_line = 1
+import numpy as np
+import numpy.matlib
+A = [[1,2],[3,4]]
+A = np.array(A)
+print(A)
+meanA = np.mean(A,0)
+print(meanA)
+rows = len(A)
+Xr = np.matlib.repmat(meanA, rows, 1)
+print(Xr)
