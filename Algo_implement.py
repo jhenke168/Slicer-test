447e6bda49f400eff11c395c2da9b4ffb37c66b8
Julia Henke <jhenke2022@gmail.com>
2020-10-21 16:05:42 -0400

    .iv points and coords into array

--- Algo_implement.py
+++ Algo_implement.py
@@ -16,13 +16,21 @@ for i in range(len(mc_coord)):
         row[j] = float(row[j])
     mc_coord_array.append(row)
 
+mc_mesh_coords = []
+for i in range(18589,55744): 
+    row = mc_mesh[i].split()
+    for j in range(len(row)):
+        row[j] = row[j].strip(',')
+        row[j] = float(row[j])
+    mc_mesh_array.append(row)
+
 mc_mesh_array = []
-for i in range(6,18585): #range(len(mc_mesh)):
+for i in range(6,18585): 
     row = mc_mesh[i].split(',')
     row = row[0].split()
     for j in range(len(row)):
         row[j] = float(row[j])
-    mc_mesh_array.append(row)
+    mc_mesh_coords.append(row)
 
 mc_mesh_array = np.array(mc_mesh_array)
 x = mc_mesh_array[:,0]
