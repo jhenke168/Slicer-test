ca4ef25f91f46595080ccd598c39e79e0e0adfb1
Julia Henke <jhenke2022@gmail.com>
2020-10-26 19:45:24 -0400

    truth value is ambiguous error

--- Algo_implement.py
+++ Algo_implement.py
@@ -1,5 +1,5 @@
 #goal: read files and display
-#need to install plotly==4.11.0
+
 import plotly.graph_objects as go
 import numpy as np
 import matplotlib
@@ -20,43 +20,42 @@ for i in range(len(mc_coord)):
     mc_coord_array.append(row)
 
 mc_mesh_coords = []
-for i in range(18589,55744): 
+for i in range(18588,55744): 
     row = mc_mesh[i].split()
     for j in range(len(row)):
         row[j] = row[j].strip(',')
-        row[j] = float(row[j])
+        row[j] = float(row[j]) #python starts at 0
     mc_mesh_coords.append(row)
 
 mc_mesh_points = []
-for i in range(6,18585): 
+for i in range(5,18585): 
     row = mc_mesh[i].split(',')
     row = row[0].split()
     for j in range(len(row)):
         row[j] = float(row[j])
     mc_mesh_points.append(row)
 
+
 mc_mesh_points = np.array(mc_mesh_points)
 x = mc_mesh_points[:,0]
-#x_t = np.transpose[x]
 y = mc_mesh_points[:,1]
-#y_t = np.transpose[y]
 z = mc_mesh_points[:,2]
 mc_mesh_coords = np.array(mc_mesh_coords)
 i = mc_mesh_coords[:,0]
-i_t = np.transpose(i)
+#i_t = np.transpose(i)
 j = mc_mesh_coords[:,1]
-j_t = np.transpose(j)
+#j_t = np.transpose(j)
 k = mc_mesh_coords[:,2]
-k_t = np.transpose(k)
+#k_t = np.transpose(k)
 #i'm pretty sure the coordinate index corresponds with i,j,k in plotly but there are obviously issues
 
-#fig = go.Figure(data=[go.Mesh3d(x=x, y=y, z=z, color='lightpink', opacity=0.50,  i=i_t, j=j_t, k=k_t)]) #, i=i, j=j, k=k
+#fig = go.Figure(data=[go.Mesh3d(x=x, y=y, z=z, color='lightpink', opacity=0.50,  i=i, j=j, k=k)]) #, i=i, j=j, k=k
 #fig.show()
 
 triangles = mc_mesh_coords[:, 0:3]
-triang = matplotlib.tri.Triangulation(x,y, triangles)
+triang = matplotlib.tri.Triangulation(x,y, triangles=triangles)
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-ax.plot_trisurf(triang, x, y, z, linewidth=0.2, antialiased=True)
+ax.plot_trisurf(triang, x, y, z, linewidth=0.2)
 plt.show()
